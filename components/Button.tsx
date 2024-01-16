72a1e6cb3e2b1c720f30a442cbbe20ab681ecdfd
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-01-16 02:33:39 +0100

    header layout

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+   ({ className, children, disabled, type = "button", ...props }, ref) => {
+      return (
+         <button type={type} disabled={disabled} ref={ref} {...props}
+         className={twMerge(`w-full rounded-full bg-green-500 text-black font-bold border border-transparent px-3 py-3 hover:opacity-75 transition disabled:cursor-not-allowed disabled:opacity-50
+         `, className)}>
+            {children}
+         </button>
+      )
+   }
+)
+
+Button.displayName = "Button"
+
+export default Button
--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,17 @@
 import SidebarItem from './SidebarItem';
 import Sidebar from './Sidebar';
+import ListItem from './ListItem';
 import Library from './Library';
+import Header from './Header';
+import Button from './Button';
 import Box from './Box';
 
 export {
    SidebarItem,
    Sidebar,
+   ListItem,
    Library,
+   Header,
+   Button,
    Box,
 }
