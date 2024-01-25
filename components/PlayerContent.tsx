78d56aeb6e7160676e205219456205b7b216dabe
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-01-25 22:25:43 +0100

    prev & next songs

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useState } from 'react'
 import { Song } from '@/types'
 import { LikeButton, MediaItem, Slider } from '@/components'
 import { BsPauseFill, BsPlayFill } from 'react-icons/bs'
 import { AiFillStepBackward, AiFillStepForward } from 'react-icons/ai'
 import { HiSpeakerWave, HiSpeakerXMark } from 'react-icons/hi2'
+import usePlayer from '@/hooks/usePlayer'
 
 interface PlayerContentProps {
    song: Song;
@@ -12,8 +14,34 @@ interface PlayerContentProps {
 }
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
-   const Icon = true ? BsPauseFill : BsPlayFill;
-   const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+   const player = usePlayer();
+   const [volume, setVolume] = useState(1);
+   const [isPlaying, setIsPlaying] = useState(false);
+
+   const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+   const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+   const onPlayNext = () => {
+      if (player.ids.length === 0) { return; }
+
+      const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+      const nextSong = player.ids[currentIndex + 1];
+
+      if (!nextSong) { return player.setId(player.ids[0]); }
+
+      player.setId(nextSong);
+   };
+
+   const onPlayPrev = () => {
+      if (player.ids.length === 0) { return; }
+
+      const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+      const prevSong = player.ids[currentIndex - 1];
+
+      if (!prevSong) { return player.setId(player.ids[player.ids.length - 1]); }
+
+      player.setId(prevSong);
+   };
 
    return (
      <div className='grid grid-cols-2 md:grid-cols-3 h-full'>
@@ -32,11 +60,11 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
        </div>
        {/* Middle Desktop Controls */}
        <div className='hidden md:flex h-full justify-center items-center w-full max-w-[722px] gap-x-6'>
-         <AiFillStepBackward onClick={() => {}} size={30} className='text-neutral-400 hover:text-white transition cursor-pointer' />
+         <AiFillStepBackward onClick={onPlayPrev} size={30} className='text-neutral-400 hover:text-white transition cursor-pointer' />
          <div onClick={() => {}} className='flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer'>
             <Icon size={30} className='text-black' />
          </div>
-         <AiFillStepForward onClick={() => {}} size={30} className='text-neutral-400 hover:text-white transition cursor-pointer' />
+         <AiFillStepForward onClick={onPlayNext} size={30} className='text-neutral-400 hover:text-white transition cursor-pointer' />
        </div>
        {/* Right Desktop Volume & Slider */}
        <div className='hidden md:flex w-full justify-end pr-2'>
